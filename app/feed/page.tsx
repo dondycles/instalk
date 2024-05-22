7176d372fe5626a540465de01ed55887678d17f4
dondycles <johnrod.dondoyano@gmail.com>
2024-05-22 18:34:23 +0800

    modified:   app/feed/page.tsx

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -10,31 +10,38 @@ import FeedNav from "./nav";
 import { ScrollArea } from "@/components/ui/scroll-area";
 
 export default function Feed() {
-  const { data: feedData } = useQuery({
-    queryKey: ["feed"],
-    queryFn: async () => await getPosts(),
-  });
   const { data: userData } = useQuery({
     queryKey: ["user"],
     queryFn: async () => await getUser(),
   });
+
+  const { data: feedData, isLoading: feedDataLoading } = useQuery({
+    queryKey: ["feed"],
+    queryFn: async () => await getPosts(),
+  });
   return (
     <main className="p-4 w-full h-full flex flex-col gap-4">
       <FeedNav />
-      <ScrollArea className="w-full flex-1">
-        <PostForm />
-        <div className="flex flex-col gap-4 mt-4">
-          {feedData?.data?.map((post) => {
-            return (
-              <PostCard
-                key={post.id + "post"}
-                post={post}
-                user={userData?.data}
-              />
-            );
-          })}
-        </div>
-      </ScrollArea>
+      {feedDataLoading ? (
+        <p className="text-center text-muted-foreground text-sm">
+          Getting the latest posts...
+        </p>
+      ) : (
+        <ScrollArea className="w-full flex-1">
+          <PostForm />
+          <div className="flex flex-col gap-4 mt-4">
+            {feedData?.data?.map((post) => {
+              return (
+                <PostCard
+                  key={post.id + "post"}
+                  post={post}
+                  user={userData?.data}
+                />
+              );
+            })}
+          </div>
+        </ScrollArea>
+      )}
     </main>
   );
 }
